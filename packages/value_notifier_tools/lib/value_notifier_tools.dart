83b4261ab3f1dd3676812c7352003db38d0b7d70
Tim Lehmenn <hi@timcreated.it>
2024-04-06 00:18:10 +0200

    fix(value_notifier_tools): Added all classes to package exports

--- packages/value_notifier_tools/lib/value_notifier_tools.dart
+++ packages/value_notifier_tools/lib/value_notifier_tools.dart
@@ -1,4 +1,9 @@
 /// Helpful lightweight tools for working with ValueNotifiers
 library value_notifier_tools;
 
+export 'src/history_value_notifier/history_value_notifier.dart';
+export 'src/history_value_notifier/history_value_notifier_mixin.dart';
 export 'src/select_value_notifier/select_value_notifier.dart';
+export 'src/where_value_notifier/where_value_notifier.dart';
+export 'src/where_value_notifier/where_value_notifier_from_parent.dart';
+export 'src/where_value_notifier/where_value_notifier_mixin.dart';
